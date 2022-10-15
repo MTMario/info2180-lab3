15dcd47d9c2fd9f5c4b8bef51a40eab59f1af4af
Mario Thompson <mariothompson181998@gmail.com>
2022-10-14 23:48:25 -0500

    Updated js file

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,7 +1,50 @@
 window.onload =  function() {
-    //let board = document.getElementsByClassName("board");
-    let squares = document.querySelectorAll("#board div");
-    for (let sqr in squares) {
+
+     let squares = document.querySelectorAll("#board div");
+     for (let sqr = 0; sqr <squares.length; sqr++ ) {
         squares[sqr].classList.add("square");
+      }
+
+    let playerX = "X";
+    let playerO = "O";
+    let whoseTurn = playerX
+
+    let boardSqr = Array(squares.length);
+    boardSqr.fill(null);
+
+
+    const gameStart = () => {
+        squares.forEach(square => square.addEventListener("click", clicked))
     }
+
+    function clicked(e) {
+        if(!boardSqr[0]) {
+            boardSqr[0] = whoseTurn
+            e.target.innerText = whoseTurn
+            whoseTurn = whoseTurn == playerX ? playerO : playerX
+        }
+    }
+
+    // let newGame = document.getElementsByClassName("btn")
+    // newGame.addEventListener("click", newgame)
+
+    
+    // function newgame()
+
+
+    gameStart()
+
+
+
+    // const winner = [
+    //     [0, 1, 2],
+    //     [3, 4, 5],
+    //     [6, 7, 8],
+    //     [0, 3, 6],
+    //     [1, 4, 7],
+    //     [2, 5, 8],
+    //     [0, 4, 8],
+    //     [2, 4, 6]
+    //   ];
+
 }
